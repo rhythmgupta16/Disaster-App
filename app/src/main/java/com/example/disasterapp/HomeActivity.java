e8f04fac88cb3a7ee20a042d16b8a5a01e6698ed
rhythmgupta16 <rhythmgupta1234@gmail.com>
2020-01-22 14:52:37 +0530

    Added Fragments

--- app/src/main/java/com/example/disasterapp/HomeActivity.java
+++ app/src/main/java/com/example/disasterapp/HomeActivity.java
@@ -2,41 +2,62 @@ package com.example.disasterapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.MenuItem;
 import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        loadFragment(new HomeFragment());
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.action_favorites:
-                                Toast.makeText(getApplicationContext(), "Favorites", Toast.LENGTH_SHORT).show();
-                                break;
-                            case R.id.action_home:
-                                Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
-                                break;
-                            case R.id.action_profile:
-                                Intent inentProfile = new Intent(HomeActivity.this, ProfileActivity.class);
-                                startActivity(inentProfile);
-                                break;
-                        }
-                        return true;
-                    }
-                });
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
+
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.action_favorites:
+                fragment = new FavoritesFragment();
+                break;
+
+            case R.id.action_profile:
+                Intent intentProfile = new Intent(HomeActivity.this, ProfileActivity.class);
+                startActivity(intentProfile);
+                break;
+
+        }
+
+        return loadFragment(fragment);
+}
 }
