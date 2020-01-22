c6524c2de956f1b370072e15fe5f2579232bbb3f
rhythmgupta16 <rhythmgupta1234@gmail.com>
2020-01-22 15:34:41 +0530

    Pick contact added

--- app/src/main/java/com/example/disasterapp/FavoritesFragment.java
+++ app/src/main/java/com/example/disasterapp/FavoritesFragment.java
@@ -1,17 +1,77 @@
 package com.example.disasterapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 public class FavoritesFragment extends Fragment {
+    private static final int RESULT_PICK_CONTACT = 0;
+    private TextView tvPhone, tvName;
+    private Button btnPick;
+    
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_favorites, null);
+        View view = inflater.inflate(R.layout.fragment_favorites, null);
+
+        btnPick = view.findViewById(R.id.btnPick);
+        tvPhone = view.findViewById(R.id.tvPhone);
+        tvName = view.findViewById(R.id.tvName);
+
+        btnPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // check whether the result is ok
+        if (resultCode == RESULT_OK) {
+            // Check for the request code, we might be usign multiple startActivityForReslut
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    Cursor cursor = null;
+                    try {
+                        String phoneNo = null ;
+                        String name = null;
+                        Uri uri = data.getData();
+                        cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+                        cursor.moveToFirst();
+                        String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+                        int  phoneIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                        phoneNo = cursor.getString(phoneIndex);
+
+                        tvPhone.setText(phoneNo);
+                        tvName.setText(contactName);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        } else {
+            Log.e("MainActivity", "Failed to pick contact");
+        }
     }
 }
--- app/src/main/java/com/example/disasterapp/HomeFragment.java
+++ app/src/main/java/com/example/disasterapp/HomeFragment.java
@@ -6,14 +6,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
+    private TextView tvText;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,container, false);
+
+        tvText = view.findViewById(R.id.tvText);
 
 
 
-        return inflater.inflate(R.layout.fragment_home,container, false);
+        return view;
     }
 }
