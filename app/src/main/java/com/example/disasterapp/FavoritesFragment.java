f9e8b58c5a3ac29945a488dc7af7c51d9a62aebb
rhythmgupta16 <rhythmgupta1234@gmail.com>
2020-01-23 21:01:45 +0530

    Get current location done

--- app/src/main/java/com/example/disasterapp/FavoritesFragment.java
+++ app/src/main/java/com/example/disasterapp/FavoritesFragment.java
@@ -1,20 +1,27 @@
 package com.example.disasterapp;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
 import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,6 +31,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -42,6 +56,10 @@ public class FavoritesFragment extends Fragment {
     String message;
     private ContactInfo contact;
     private ArrayList<ContactInfo> contactList = new ArrayList<ContactInfo>();
+    int PERMISSION_ID = 44;
+    FusedLocationProviderClient mFusedLocationClient;
+    TextView latTextView, lonTextView;
+    String latitude, longitude;
 
     @Nullable
     @Override
@@ -54,10 +72,16 @@ public class FavoritesFragment extends Fragment {
         tvName = view.findViewById(R.id.tvName);
         tvPicked = view.findViewById(R.id.tvPicked);
         btnSend = view.findViewById(R.id.btnSend);
+        latTextView = view.findViewById(R.id.latTextView);
+        lonTextView = view.findViewById(R.id.longTextView);
         btnSend.setVisibility(View.INVISIBLE);
         tvPhone.setVisibility(View.INVISIBLE);
         tvName.setVisibility(View.INVISIBLE);
         tvPicked.setVisibility(View.INVISIBLE);
+
+        //Get Current Coordinates
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        getLastLocation();
 
 
         btnPick.setOnClickListener(new View.OnClickListener() {
@@ -90,7 +114,8 @@ public class FavoritesFragment extends Fragment {
                     }
 
                     for(String number : numbers) {
-                        message = "URGENT! Please help me.";
+                        message = "URGENT! I am in danger! My current location is latitude: "+ latitude +
+                                " longitude: " + longitude + ". Send help ASAP.";
                         sms.sendTextMessage(number, null, message, null, null);
                         Log.i(TAG, "onClick: SENT to "+ number + " with message: " + message);
                     }
@@ -147,5 +172,107 @@ public class FavoritesFragment extends Fragment {
         }
     }
 
+    //Location
+    @SuppressLint("MissingPermission")
+    private void getLastLocation(){
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(
+                        new OnCompleteListener<Location>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                Location location = task.getResult();
+                                if (location == null) {
+                                    requestNewLocationData();
+                                } else {
+                                    latTextView.setText(location.getLatitude()+"");
+                                    latitude = location.getLatitude()+"";
+                                    lonTextView.setText(location.getLongitude()+"");
+                                    longitude = location.getLongitude()+"";
 
+                                }
+                            }
+                        }
+                );
+            } else {
+                Toast.makeText(getActivity(), "Turn on location", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData(){
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                Looper.myLooper()
+        );
+
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latTextView.setText(mLastLocation.getLatitude()+"");
+            latitude = mLastLocation.getLatitude()+"";
+            lonTextView.setText(mLastLocation.getLongitude()+"");
+            longitude = mLastLocation.getLongitude()+"";
+        }
+    };
+
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(
+                getActivity(),
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_ID
+        );
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+
+    }
 }
+
